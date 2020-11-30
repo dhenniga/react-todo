2b87099d71fec288c4b7fe6885dbf73a52d75ec4
David Hennigan <david@fluidmotion.ie>
2020-11-30 11:40:59 +0000

    Pc push

--- src/task/task.styled.js
+++ src/task/task.styled.js
@@ -1,5 +1,28 @@
 import styled, { css } from "styled-components";
 
+export const DeleteButton = styled.button`
+  background-color: red;
+  height: 16px;
+  width: 16px;
+  display: grid;
+  border-radius: 3px;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 13px;
+  right: 0px;
+  border: none;
+  outline: 0px;
+  color: white;
+  cursor: pointer;
+  font-family: sans-serif;
+  line-height: 0px;
+  font-weight: 400;
+  font-size: 9pt;
+  opacity: 0;
+  transition: opacity 0.3s cubic-bezier(0,0,0,1);
+`;
+
 export const Container = styled.div`
   height: 40px;
   width: 100%;
@@ -8,9 +31,13 @@ export const Container = styled.div`
   grid-template-columns: 40px 1fr;
   align-content: center;
   border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  transition: border-bottom-color 0.3s cubic-bezier(0,0,0,1);
 
   &:hover {
-    background-color: rgba(0, 0, 0, 0.03);
+    border-bottom-color: rgb(255,94,19);
+    ${DeleteButton} {
+      opacity: 1 !important;
+    }
   }
 `;
 
@@ -37,28 +64,3 @@ export const Input = styled.input`
 `;
 
 export const Checkbox = styled.input``;
-
-export const DeleteButton = styled.button`
-  background-color: red;
-  height: 16px;
-  width: 16px;
-  display: grid;
-  border-radius: 3px;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  top: 13px;
-  right: 0px;
-  border: none;
-  outline: 0px;
-  color: white;
-  cursor: pointer;
-  font-family: sans-serif;
-  font-weight: 900;
-  font-size: 9pt;
-  opacity: 0.2;
-
-  &:hover {
-    opacity: 1;
-  }
-`;
